a43795ccceb8c108a065cc6042a863e1d8be2e95
Rizky Mahendra <56747067+Rizkyemha@users.noreply.github.com>
2025-07-20 16:47:15 +0700

    postgres database implemented

---
+++ src/server.js
@@ -0,0 +1,51 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-tabs */
+/* eslint-disable indent */
+/* eslint-disable quotes */
+
+require("dotenv").config();
+const Hapi = require("@hapi/hapi");
+const notes = require("./api/notes");
+const NotesValidator = require("./validator/notes");
+const NotesService = require("./services/postgres/NotesService");
+
+const init = async () => {
+	const notesService = new NotesService();
+
+	const server = Hapi.server({
+		port: process.env.PORT,
+		host: process.env.HOST,
+		routes: {
+			cors: {
+				origin: ["*"],
+			},
+		},
+	});
+
+	await server.register({
+		plugin: notes,
+		options: {
+			service: notesService,
+			validator: NotesValidator,
+		},
+	});
+
+	server.ext("onPreResponse", (request, h) => {
+		const { response } = request;
+
+		if (response instanceof Error) {
+			const newError = h.response({
+				status: "fail",
+				message: response.message,
+			});
+			newError.code(response.statusCode);
+			return newError;
+		}
+		return h.continue;
+	});
+
+	await server.start();
+	console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
