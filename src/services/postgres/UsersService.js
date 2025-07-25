e8c91a42bd7b55f848e8df2e7042aaf62ae80a9a
Rizky Mahendra <56747067+Rizkyemha@users.noreply.github.com>
2025-07-25 16:38:26 +0700

    create users service

---
+++ src/services/postgres/UsersService.js
@@ -0,0 +1,66 @@
+const { nanoid } = require("nanoid");
+const { Pool } = require("pg");
+const bcrypt = require("bcrypt");
+const InvariantError = require("../../api/notes/exceptions/InvariantError");
+const NotFoundError = require("../../api/notes/exceptions/NotFoundError");
+
+class UserService {
+	constructor() {
+		this._pool = new Pool();
+
+		this.addUser = this.addUser.bind(this);
+		this.getUserById = this.getUserById(this);
+	}
+
+	async verifyNewUsername(username) {
+		const query = {
+			text: "SELECT username FROM users WHERE username = $1",
+			values: [username],
+		};
+
+		const result = await this._pool.query(query);
+
+		if (result.rows.length) {
+			throw new InvariantError(
+				"Gagal menambahkan user. Username sudah digunakan."
+			);
+		}
+	}
+
+	async addUser({ username, password, fullname }) {
+		await this.verifyNewUsername(username);
+
+		const id = nanoid(16);
+		const hashedPassword = await bcrypt.hash(password, 10);
+
+		const query = {
+			text: "INSERT INTO users (id, username, password, fullname) VALUES ($1, $2, $3, $4) RETURNING id",
+			value: [id, username, hashedPassword, fullname],
+		};
+
+		const result = await this._pool.query(query);
+
+		if (!result.rows.length) {
+			throw new InvariantError("User gagal ditambahkan");
+		}
+
+		return result.rows[0].id;
+	}
+
+	async getUserById(id) {
+		const query = {
+			text: "SELECT id, username, fullname FROM users WHERE id = $1",
+			values: [id],
+		};
+
+		const result = await this._pool.query(query);
+
+		if (!result.rows.length) {
+			throw new NotFoundError("User tidak ditemukan");
+		}
+
+		return result.rows[0];
+	}
+}
+
+module.exports = UserService;
