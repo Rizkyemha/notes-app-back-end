01e6c6b8aaf8f02e800d22ef47533f417a5ad433
Rizky Mahendra <56747067+Rizkyemha@users.noreply.github.com>
2025-07-28 16:59:24 +0700

    create schema and validator authentications payload

---
+++ src/validator/authentications/index.js
@@ -0,0 +1,31 @@
+const InvariantError = require("../../api/notes/exceptions/InvariantError");
+const {
+	PostAuthenticationsPayloadSchema,
+	PutAuthenticationsPayloadSchema,
+	DeleteAuthenticationsPayloadSchema,
+} = require("./schema");
+
+const AuthenticationsValidator = {
+	validatePostAuthenticationsPayload: (payload) => {
+		const validationResult =
+			PostAuthenticationsPayloadSchema.validate(payload);
+		if (validationResult.error) {
+			throw new InvariantError(validationResult.error.message);
+		}
+	},
+	validatePutAuthenticationsPayload: (payload) => {
+		const validateResult = PutAuthenticationsPayloadSchema.validate(payload);
+		if (validateResult.error) {
+			throw new InvariantError(validateResult.error.message);
+		}
+	},
+	validateDeleteAuthenticationsPayload: (payload) => {
+		const validateResult =
+			DeleteAuthenticationsPayloadSchema.validate(payload);
+		if (validateResult.error) {
+			throw new InvariantError(validateResult.error.message);
+		}
+	},
+};
+
+module.exports = AuthenticationsValidator;
