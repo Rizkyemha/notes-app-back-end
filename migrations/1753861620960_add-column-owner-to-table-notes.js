5a04ff8f8cb15d68d56232e581275bc086c85afa
Rizky Mahendra <56747067+Rizkyemha@users.noreply.github.com>
2025-07-30 14:48:11 +0700

    add column owner to table notes

--- migrations/1753861620960_add-column-owner-to-table-notes.js
+++ migrations/1753861620960_add-column-owner-to-table-notes.js
@@ -1,18 +1,11 @@
-/**
- * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
- */
-export const shorthands = undefined;
+exports.up = (pgm) => {
+	pgm.addColumn("notes", {
+		owner: {
+			type: "VARCHAR(50)",
+		},
+	});
+};
 
-/**
- * @param pgm {import('node-pg-migrate').MigrationBuilder}
- * @param run {() => void | undefined}
- * @returns {Promise<void> | void}
- */
-export const up = (pgm) => {};
-
-/**
- * @param pgm {import('node-pg-migrate').MigrationBuilder}
- * @param run {() => void | undefined}
- * @returns {Promise<void> | void}
- */
-export const down = (pgm) => {};
+exports.down = (pgm) => {
+	pgm.dropColumn("notes", "owner");
+};
